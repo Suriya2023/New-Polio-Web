f4ed303fdc7c7e4c375547a04973fd377575e808
Suriya Singh Rajput <148303377+Suriya2023@users.noreply.github.com>
2024-05-24 09:16:03 +0530

    New Polio WebSite
    I Have Created New web project in Landing Page!

---
+++ polio.js
@@ -0,0 +1,8 @@
+
+        // let circle = document.querySelector(".circle");
+        // document.addEventListener("mousemove" , (e) => {
+        //     circle.style.top = e.clientY + "px";
+        //     circle.style.left = e.clientX + "px";
+            
+
+        // });
